a20854c9bf4c87944685156b43cb4e14f7594855
tanaka <tanaka@logical-studio.com>
2019-09-27 08:05:51 +0900

    make-todo-base

--- js/main.js
+++ js/main.js
@@ -4,11 +4,23 @@
     var vm = new Vue({
         el: '#app',
         data: {
+            newItem: '',
             todos: [
                 'task1',
                 'task2',
                 'task3',
             ]
+        },
+        methods: {
+            addItem: function(e) {
+                if (this.newItem) {
+                    // e.preventDefault();
+                    this.todos.push(this.newItem);
+                    this.newItem = '';
+                } else {
+                    console.log('エラー');
+                }
+            }
         }
     });
 })();
